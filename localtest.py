486b4172993110aa391c3ee856578a057f2e27da
ksw <zenuine1@gmail.com>
2019-02-12 20:46:59 +0900

    localtest.py 삭제

--- localtest.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import socket
-
-cmd = "ifconfig ens3 | grep Mask | cut -d: -f 2 | awk '{print$1}'"
-result = subprocess.check_output([cmd], shell=True, universal_newlines=True)
-soc = socket.gethostbyname(socket.getfqdn())
-
-print(result)
-print(soc)
